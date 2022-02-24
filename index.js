b436658b7bb68e37f837ac3fa392135b7eb7cd0b
Betelihem <Betelihem>
2022-02-24 14:54:46 -0800

    commit tree cbfdbccfd75bc410089909ca9d925984607ecb38

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
+/* what is the algorithm problem in your own Words
+- write a function which reads a string and compared itself forward and backwards (Palindrome)
+- returns true if the string is the same forwards and backward and returns false if it isn't
+*/
+
 function isPalindrome(word) {
   // Write your algorithm here
+  //1 convert the string to array
+  const wordArr = Array.from(word)
+  console.log("wordArr:", wordArr)
+  // 2 reverse array
+  const reversed = wordArr.reverse()
+  // 3 convert back to string
+  const backwards = reversed.join('')
+  // 4 compare reverse string to the original string
+  // 5 return a boolean of the comparison
+  return word = backwards
 }
 
 /* 
   Add your pseudocode here
+  1- convert the string to array
+  2- reverse array
+  3- convert back to string
+  4- compare reverse string to the original string
+  5- return a boolean of the comparison
 */
 
 /*
@@ -16,6 +36,9 @@ if (require.main === module) {
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
   console.log("");
 
   console.log("Expecting: false");
@@ -23,3 +46,7 @@ if (require.main === module) {
 }
 
 module.exports = isPalindrome;
+
+
+// run     node index.js   in terminal to test
+// solution above might not be complete
